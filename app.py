694bb88375cf3195108e99abb20667919e4def0d
Roberto Barreto <robertofmbarreto@gmail.com>
2024-11-08 21:23:12 +0000

    fix

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+@app.route('/calculate')
+def calculator():
+    num1=request.args.get('a', default=0, type=int)
+    num2=request.args.get("b", default=0, type=int)
+    operation=request.args.get("op")
+    if operation == 'add':
+        return str(num1 + num2)
+    elif operation == 'sub':
+        return str(num1 - num2)
+    elif operation == 'mul':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    else:
+        return "Invalid operation"
 if __name__ == '__main__':
     app.run()
